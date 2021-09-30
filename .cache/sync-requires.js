69c515ca9edb79943789db3c8b06861a550f3e6f
[Nicola Fontebasso] <[nicola.fontebasso@gmail.com]>
2021-09-30 14:12:52 +0200

    First commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/nicolafontebasso/Desktop/Projects/2mwood-gatsby/2mwood-gatsby-tailwind/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/nicolafontebasso/Desktop/Projects/2mwood-gatsby/2mwood-gatsby-tailwind/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/nicolafontebasso/Desktop/Projects/2mwood-gatsby/2mwood-gatsby-tailwind/src/pages/index.js"))
+}
+
